cf58e24ebe9631e651b171ca715fdc8ba7dff579
Olzhas Askar <olzhas.askar@gmail.com>
2018-01-12 00:14:49 +0100

    Added game page

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def index():
 def log():
     return render_template("log.html")
 
+@app.route("/game")
+def game():
+    return render_template("game.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port)
---
+++ static/components/game.jsx
@@ -0,0 +1,113 @@
+class Square extends React.Component {
+  allowDrop(e) {
+    e.preventDefault();
+  }
+  onDrop(e) {
+    let draggedId = e.dataTransfer.getData('text/html');
+    let newContent =  document.getElementById(draggedId);
+    console.log("Before: " + newContent.parentNode.id);
+    e.target.appendChild(newContent);
+    console.log("After: " + newContent.parentNode.id);
+  }
+  render() {
+    return (
+      <div
+        id={this.props.position}
+        className={"square unselectable " + this.props.color}
+        onDragOver={this.allowDrop}
+        onDrop={this.onDrop}>
+          {this.props.initContent}
+      </div>
+    );
+  }
+}
+
+class Row extends React.Component {
+  isNumericColumn(column) {
+    return column === 0 || column === 25;
+  }
+  isAlphaRow(row) {
+    return row === " ";
+  }
+  calculatePosition(row, column) {
+    return row + column;
+  }
+  calculateDescription(row, column) {
+    let result = "";
+    if (!this.isNumericColumn(column) && this.isAlphaRow(row)){
+        result = column;
+      } else if (this.isNumericColumn(column)){
+        result = row;
+      }
+    return result;
+  }
+  calculateColor(row, column) {
+    let result = "pitch-green-dark";
+
+    if (!this.isNumericColumn(column) && !this.isAlphaRow(row)) {
+      result = (column % 2 != row.charCodeAt(0) % 2)
+        ? "pitch-green-dark"
+        : "pitch-green-middle";
+    }
+    return result;
+  }
+  
+  render() {
+    const row = this.props.row;
+    const columns = Array.from(new Array(26), (e, i) => i).map(column =>
+    {
+      return (
+        <Square
+          position={this.calculatePosition(row, column)}
+          color={this.calculateColor(row, column)}
+          initContent={this.calculateDescription(row, column)} />
+      );
+    }
+  );
+    return <div className="row">{columns}</div>;
+  }
+}
+
+class Board extends React.Component {
+  render() {
+    const rows = ' abcdefghijklmnop '.split('').map(a => <Row row={a} />);
+    return <div>{rows}</div>;
+  }
+}
+
+class Player extends React.Component {
+  onDragStart(e) {
+    e.dataTransfer.setData("text/html", e.target.id);
+    e.dataTransfer.effectAllowed = "move";
+  }
+  render() {
+    return (
+      <div
+        id={this.props.number}
+        className="player"
+        draggable="true"
+        onDragStart={this.onDragStart} >
+          {this.props.number}
+      </div>
+      );
+  }
+}
+
+ReactDOM.render(
+  <div className="container">
+    <Board />
+  </div>
+  ,
+  document.getElementById("app")
+);
+
+let team = [
+  { number: 3, position: "b9" },
+  { number: 11, position: "h13" },
+  { number: 14, position: "m6" },
+]
+
+team.forEach(p =>
+  ReactDOM.render(<Player number={p.number} />,
+    document.getElementById(p.position)
+));
