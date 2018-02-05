b35d429d2310e5d115c78270f82ad96831a76d56
Olzhas Askar <olzhas.askar@gmail.com>
2018-02-05 16:44:17 +0100

    Initialized webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname, "./static/dist");
+var SRC_DIR = path.resolve(__dirname, "./static/components");
+
+var config = {
+    entry: SRC_DIR + "/Game.jsx",
+    output: {
+        path: DIST_DIR,
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?/,
+                exclude: /node_modules/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ["es2015", "react", "es2017"]
+                }
+            }
+        ]
+    }
+};
+
+module.exports = config;
