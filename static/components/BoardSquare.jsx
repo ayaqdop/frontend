e51cddef2df928594380df673314b210db2c0517
Olzhas Askar <olzhas.askar@gmail.com>
2018-02-13 23:15:32 +0100

    Renamed Row to BoardSquare

---
+++ static/components/BoardSquare.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Square from "./Square.jsx";
+import { calculatePosition, calculateColor, calculateDescription } from "./Helpers";
+
+export default class BoardSquare extends React.Component {
+  render() {
+    const row = this.props.row;
+    const columns = Array.from(new Array(26), (e, i) => i).map(column =>
+    {
+      return (
+        <Square
+          key={row + column}
+          position={calculatePosition(row, column)}
+          color={calculateColor(row, column)}
+          initContent={calculateDescription(row, column)} />
+        );
+      }
+    );
+    return <div className="row">{columns}</div>;
+  }
+}
---
+++ static/components/Helpers.js
@@ -0,0 +1,29 @@
+function isNumericColumn(column) {
+  return column === 0 || column === 25;
+}
+function isAlphaRow(row) {
+  return row === " ";
+}
+
+export function calculatePosition(row, column) {
+  return row + column;
+}
+export function calculateDescription(row, column) {
+  let result = "";
+  if (!isNumericColumn(column) && isAlphaRow(row)){
+      result = column;
+    } else if (isNumericColumn(column)){
+      result = row;
+    }
+  return result;
+}
+export function calculateColor(row, column) {
+  let result = "pitch-green-dark";
+
+  if (!isNumericColumn(column) && !isAlphaRow(row)) {
+    result = (column % 2 != row.charCodeAt(0) % 2)
+      ? "pitch-green-dark"
+      : "pitch-green-middle";
+  }
+  return result;
+}
