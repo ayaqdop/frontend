b4b1f8920308dae8f1db1a2f9347d31ec5873382
Olzhas Askar <olzhas.askar@gmail.com>
2018-04-01 01:54:48 +0200

    Added real Barcelona and Bayern formations

--- static/components/Game.js
+++ static/components/Game.js
@@ -7,11 +7,17 @@ let gameObjects = {
 		score: 0,
 		moves: 0,
 		players: [
-			{ number: 14, position: [5, 5] },
-			{ number: 4, position: [10, 10] },
-			{ number: 6, position: [10, 5] },
-			{ number: 8, position: [5, 10] },
-			{ number: 10, position: [1, 4] },
+			{ number: 1,  position: [1, 8] },
+			{ number: 18, position: [6, 3] },
+			{ number: 23, position: [4, 6] },
+			{ number: 3,  position: [4, 10] },
+			{ number: 20, position: [6, 14] },
+			{ number: 8,  position: [10, 5] },
+			{ number: 5,  position: [8, 8] },
+			{ number: 4,  position: [10, 11] },
+			{ number: 14, position: [12, 4] },
+			{ number: 9,  position: [12, 8] },
+			{ number: 10, position: [12, 12] },
 		]
 	},
 	{
@@ -19,8 +25,17 @@ let gameObjects = {
 		score: 0,
 		moves: 0,
 		players: [
-			{ number: 22, position: [23, 5] },
-			{ number: 24, position: [22, 10] },
+			{ number: 1,  position: [24, 9] },
+			{ number: 32, position: [19, 14] },
+			{ number: 5,  position: [22, 7] },
+			{ number: 17, position: [22, 10] },
+			{ number: 27, position: [19, 3] },
+			{ number: 25, position: [16, 4] },
+			{ number: 6,  position: [18, 9] },
+			{ number: 11, position: [16, 12] },
+			{ number: 10, position: [13, 13] },
+			{ number: 9,  position: [15, 8] },
+			{ number: 7,  position: [13, 3] },
 		]
 	}]
 };
