ce24621d599a3db4f9040c8e5a094e0c3878298e
Olzhas Askar <olzhas.askar@gmail.com>
2018-04-03 23:35:14 +0200

    Failing test

--- tests/Helpers.test.js
+++ tests/Helpers.test.js
@@ -3,3 +3,7 @@ import { range } from "../static/components/Helpers";
 test("range 3..7", () => {
   expect(range(3, 7)).toEqual([3, 4, 5, 6, 7]);
 });
+
+test("range 3..0", () => {
+  expect(range(3, 0)).toEqual([3, 2, 1, 0]);
+});
