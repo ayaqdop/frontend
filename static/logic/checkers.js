d5558aff123aa6cef049583c95e9eb0239489434
Olzhas Askar <olzhas.askar@gmail.com>
2018-04-06 23:19:46 +0200

    Refactored checker methods into their own class

---
+++ static/logic/checkers.js
@@ -0,0 +1,44 @@
+import { range } from "./Helpers";
+
+export function canMoveBall(all, fromPosition, toPosition) {
+  const filtered = removeSelf(all, fromPosition);
+
+	return canMoveVertically(filtered, fromPosition, toPosition)
+		|| canMoveHorizontally(filtered, fromPosition, toPosition)
+		|| canMoveDiagonally(filtered, fromPosition, toPosition);
+};
+
+
+function removeSelf(all, fromPosition) {
+  return all.filter(p => (p[0] !== fromPosition[0] && p[1] !== fromPosition[1])
+    || (p[0] === fromPosition[0] && p[1] !== fromPosition[1])
+    || (p[0] !== fromPosition[0] && p[1] === fromPosition[1]));
+}
+
+function canMoveVertically(filteredPositions, fromPosition, toPosition) {
+  const [ fromX, fromY ] = fromPosition;
+  const [ toX, toY ] = toPosition;
+
+  return fromX === toX
+    && range(fromY, toY).every(y => !filteredPositions.some(p => p[0] === toX && p[1] === y));
+}
+function canMoveHorizontally(filteredPositions, fromPosition, toPosition) {
+  const [ fromX, fromY ] = fromPosition;
+  const [ toX, toY ] = toPosition;
+
+  return fromY === toY
+    && range(fromX, toX).every(x => !filteredPositions.some(p => p[0] === x && p[1] === toY));
+}
+function canMoveDiagonally(filteredPositions, fromPosition, toPosition) {
+  const [ fromX, fromY ] = fromPosition;
+  const [ toX, toY ] = toPosition;
+
+  return (Math.abs(toY - fromY) === Math.abs(toX - fromX));
+}
+
+exports.privateFunctions = {
+  removeSelf,
+  canMoveDiagonally,
+  canMoveHorizontally,
+  canMoveVertically
+};
--- tests/logic/checkers.test.js
+++ tests/logic/checkers.test.js
@@ -1,5 +1,5 @@
-// import { } from "../../static/logic/checkers";
-import { range } from "../../static/logic/Helpers";
+import { privateFunctions } from "../../static/logic/checkers";
+
 
 const allPositions = [
 			        [1, 8],
@@ -9,7 +9,7 @@ const allPositions = [
               [8, 8],
     [10, 5],            [10, 11],
               
-[12, 4],  [12, 8], [12, 9], [12, 12],
+    [12, 4],  [12, 9], [12, 12],
 
 
     [13, 3],            [13, 13],
@@ -23,62 +23,40 @@ const allPositions = [
               [24, 9],
 ];
 
-function removeSelf(all, fromPosition) {
-  return all.filter(p => (p[0] !== fromPosition[0] && p[1] !== fromPosition[1])
-    || (p[0] === fromPosition[0] && p[1] !== fromPosition[1])
-    || (p[0] !== fromPosition[0] && p[1] === fromPosition[1]));
-}
-
-function canMoveVertically(all, fromPosition, toPosition) {
-  const filtered = removeSelf(all, fromPosition);
-  const [ fromX, fromY ] = fromPosition;
-  const [ toX, toY ] = toPosition;
-
-  return fromX === toX
-    && range(fromY, toY).every(y => !filtered.some(p => p[0] === toX && p[1] === y));
-}
-
-function canMoveHorizontally(all, fromPosition, toPosition) {
-  const filtered = removeSelf(all, fromPosition);
-  const [ fromX, fromY ] = fromPosition;
-  const [ toX, toY ] = toPosition;
-
-  return fromY === toY
-    && range(fromX, toX).every(x => !filtered.some(p => p[0] === x && p[1] === toY));
-}
+const ballPosition = [12, 8];
 
 test("remove position from an array of positions", () => {
-  expect(removeSelf([[12, 8], [12, 5]], [12, 5])).toEqual([[12, 8]]);
-  expect(removeSelf([[12, 8], [12, 5]], [12, 11])).toEqual([[12, 8], [12, 5]]);
+  expect(privateFunctions.removeSelf([ballPosition, [12, 5]], [12, 5])).toEqual([[12, 8]]);
+  expect(privateFunctions.removeSelf([ballPosition, [12, 5]], [12, 11])).toEqual([[12, 8], [12, 5]]);
 });
 
 test("can move vertically", () => {
-  expect(canMoveVertically(allPositions, [12, 8], [12, 5])).toBeTruthy();
-  expect(canMoveVertically(allPositions, [12, 9], [12, 11])).toBeTruthy();
+  expect(privateFunctions.canMoveVertically(allPositions, ballPosition, [12, 5])).toBeTruthy();
+  expect(privateFunctions.canMoveVertically(allPositions, [12, 10], [12, 11])).toBeTruthy();
 });
 
 test("can not move vertically past other objects", () => {
-  expect(canMoveVertically(allPositions, [12, 8], [12, 2])).toBeFalsy();
-  expect(canMoveVertically(allPositions, [12, 9], [12, 13])).toBeFalsy();
+  expect(privateFunctions.canMoveVertically(allPositions, ballPosition, [12, 2])).toBeFalsy();
+  expect(privateFunctions.canMoveVertically(allPositions, ballPosition, [12, 13])).toBeFalsy();
 });
 
 test("can not move vertically if the horizontal axes differ", () => {
-  expect(canMoveVertically(allPositions, [12, 8], [10, 2])).toBeFalsy();
-  expect(canMoveVertically(allPositions, [10, 9], [12, 13])).toBeFalsy();
+  expect(privateFunctions.canMoveVertically(allPositions, ballPosition, [10, 2])).toBeFalsy();
+  expect(privateFunctions.canMoveVertically(allPositions, [10, 9], [12, 13])).toBeFalsy();
 });
 
 test("can move horizontally", () => {
-  expect(canMoveHorizontally(allPositions, [8, 8], [2, 8])).toBeTruthy();
-  expect(canMoveHorizontally(allPositions, [8, 8], [11, 8])).toBeTruthy();
+  expect(privateFunctions.canMoveHorizontally(allPositions, [7, 8], [2, 8])).toBeTruthy();
+  expect(privateFunctions.canMoveHorizontally(allPositions, [9, 8], [11, 8])).toBeTruthy();
 });
 
 test("can not move horizontally past other objects", () => {
-  expect(canMoveHorizontally(allPositions, [12, 8], [1, 8])).toBeFalsy();
-  expect(canMoveHorizontally(allPositions, [1, 8], [10, 8])).toBeFalsy();
+  expect(privateFunctions.canMoveHorizontally(allPositions, ballPosition, [1, 8])).toBeFalsy();
+  expect(privateFunctions.canMoveHorizontally(allPositions, [1, 8], [10, 8])).toBeFalsy();
 });
 
 test("can not move horizontally if the horizontal axes differ", () => {
-  expect(canMoveHorizontally(allPositions, [12, 8], [10, 2])).toBeFalsy();
-  expect(canMoveHorizontally(allPositions, [10, 9], [12, 13])).toBeFalsy();
+  expect(privateFunctions.canMoveHorizontally(allPositions, ballPosition, [10, 2])).toBeFalsy();
+  expect(privateFunctions.canMoveHorizontally(allPositions, [10, 9], [12, 13])).toBeFalsy();
 });
 
