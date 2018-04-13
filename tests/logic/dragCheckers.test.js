b40b339cce1fd4fa579b20439484dc5ef025f55a
Olzhas Askar <olzhas.askar@gmail.com>
2018-04-13 23:22:59 +0200

    Added drag checkers tests

--- tests/logic/dragCheckers.test.js
+++ tests/logic/dragCheckers.test.js
@@ -1,15 +1,51 @@
 import { canDragBall, canDragPlayer } from "../../static/logic/dragCheckers";
 
+
+
 test("can not drag player, team has moves but he doesn't", () => {
-  expect(canDragPlayer(team, playerNumber)).toBe(false);
+  const team = {
+    name: "FC Barcelona",
+    score: 0,
+    moves: 5,
+    players: [
+      { number: 1,  moves: 0, position: [1, 8] },
+      { number: 18, moves: 3, position: [6, 3] },
+      { number: 23, moves: 2, position: [4, 6] },
+      { number: 3,  moves: 1, position: [4, 11] },
+    ]
+  };
+  const player = team.players[0];
+  expect(canDragPlayer(team, player)).toBe(false);
 });
 
 test("can not drag player, he has moves but his team doesn't", () => {
-  expect(canDragPlayer(team, playerNumber)).toBe(false);
+  const team = {
+    name: "FC Barcelona",
+    score: 0,
+    moves: 0,
+    players: [
+      { number: 1,  moves: 0, position: [1, 8] },
+      { number: 18, moves: 3, position: [6, 3] },
+      { number: 23, moves: 2, position: [4, 6] },
+      { number: 3,  moves: 1, position: [4, 11] },
+    ]
+  };
+  const player = team.players[1];
+  expect(canDragPlayer(team, player)).toBe(false);
 });
 
 test("can drag player", () => {
-  expect(canDragPlayer(team, playerNumber)).toBe(false);
+  const team = {
+    name: "FC Barcelona",
+    score: 0,
+    moves: 3,
+    players: [
+      { number: 1,  moves: 0, position: [1, 8] },
+      { number: 18, moves: 3, position: [6, 3] },
+      { number: 23, moves: 2, position: [4, 6] },
+      { number: 3,  moves: 1, position: [4, 11] },
+    ]
+  };
+  const player = team.players[2];
+  expect(canDragPlayer(team, player)).toBe(true);
 });
-
-
