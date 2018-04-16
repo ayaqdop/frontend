f42e5311c783fa88ed37a0a3ab34c68a2e0cfb97
Olzhas Askar <olzhas.askar@gmail.com>
2018-04-16 18:23:33 +0200

    Refactored to use describe suites

--- tests/logic/helpers.test.js
+++ tests/logic/helpers.test.js
@@ -1,67 +1,64 @@
 import * as helpers from "../../static/logic/helpers";
 
-test("range same value", () => {
-  expect(helpers.range(0, 0)).toEqual([0]);
-  expect(helpers.range(42, 42)).toEqual([42]);
-  expect(helpers.range(-42, -42)).toEqual([-42]);
+describe("range", () => {
+  test("same value", () => {
+    expect(helpers.range(0, 0)).toEqual([0]);
+    expect(helpers.range(42, 42)).toEqual([42]);
+    expect(helpers.range(-42, -42)).toEqual([-42]);
+  });
+  test("inclusive positive incremental", () => {
+    expect(helpers.range(3, 7)).toEqual([3, 4, 5, 6, 7]);
+    expect(helpers.range(0, 2)).toEqual([0, 1, 2]);
+  });
+  test("inclusive positive decremental", () => {
+    expect(helpers.range(7, 3)).toEqual([7, 6, 5, 4, 3]);
+    expect(helpers.range(2, 0)).toEqual([2, 1, 0]);
+  });
+  test("inclusive negative incremental", () => {
+    expect(helpers.range(-7, -3)).toEqual([-7, -6, -5, -4, -3]);
+    expect(helpers.range(-2, 0)).toEqual([-2, -1, 0]);
+  });
+  test("inclusive negative decremental", () => {
+    expect(helpers.range(-3, -7)).toEqual([-3, -4, -5, -6, -7]);
+    expect(helpers.range(0, -2)).toEqual([0, -1, -2]);
+  });
 });
 
-test("range inclusive positive incremental", () => {
-  expect(helpers.range(3, 7)).toEqual([3, 4, 5, 6, 7]);
-  expect(helpers.range(0, 2)).toEqual([0, 1, 2]);
-});
-
-test("range inclusive positive decremental", () => {
-  expect(helpers.range(7, 3)).toEqual([7, 6, 5, 4, 3]);
-  expect(helpers.range(2, 0)).toEqual([2, 1, 0]);
-});
-
-test("range inclusive negative incremental", () => {
-  expect(helpers.range(-7, -3)).toEqual([-7, -6, -5, -4, -3]);
-  expect(helpers.range(-2, 0)).toEqual([-2, -1, 0]);
-});
-
-test("range inclusive negative decremental", () => {
-  expect(helpers.range(-3, -7)).toEqual([-3, -4, -5, -6, -7]);
-  expect(helpers.range(0, -2)).toEqual([0, -1, -2]);
-});
-
-test("calculateDescription empty corners", () => {
-  expect(helpers.calculateDescription(helpers.MIN_COLUMN, helpers.MIN_ROW)).toBe(" ");
-  expect(helpers.calculateDescription(helpers.MIN_COLUMN, helpers.MAX_ROW)).toBe(" ");
-  expect(helpers.calculateDescription(helpers.MAX_COLUMN, helpers.MIN_ROW)).toBe(" ");
-  expect(helpers.calculateDescription(helpers.MAX_COLUMN, helpers.MAX_ROW)).toBe(" ");
-});
-
-test("calculateDescription empty field", () => {
-  for (let i of helpers.range(1, 24)) {
-    for (let j of helpers.range(1, 16)) {
-      expect(helpers.calculateDescription(i, j)).toBeFalsy();
+describe("calculate description", () => {
+  test("empty corners", () => {
+    expect(helpers.calculateDescription(helpers.MIN_COLUMN, helpers.MIN_ROW)).toBe(" ");
+    expect(helpers.calculateDescription(helpers.MIN_COLUMN, helpers.MAX_ROW)).toBe(" ");
+    expect(helpers.calculateDescription(helpers.MAX_COLUMN, helpers.MIN_ROW)).toBe(" ");
+    expect(helpers.calculateDescription(helpers.MAX_COLUMN, helpers.MAX_ROW)).toBe(" ");
+  });
+  test("empty field", () => {
+    for (let i of helpers.range(1, 24)) {
+      for (let j of helpers.range(1, 16)) {
+        expect(helpers.calculateDescription(i, j)).toBeFalsy();
+      }
     }
-  }
-});
+  });
+  test("alpha rows", () => {
+    const rows = " abcdefghijklmnop ";
 
-test("calculateDescription alpha rows", () => {
-  const rows = " abcdefghijklmnop ";
+    for (let row of helpers.range(0, 17)) {
+      let expectedLetter = rows.charAt(row);
 
-  for (let row of helpers.range(0, 17)) {
-    let expectedLetter = rows.charAt(row);
-
-    expect(helpers.calculateDescription(0, row)).toBe(expectedLetter);
-    expect(helpers.calculateDescription(25, row)).toBe(expectedLetter);
-  }
-});
-
-test("calculateDescription numeric columns", () => {
-  for (let column of helpers.range(1, 24)) {
-    expect(helpers.calculateDescription(column, 0)).toBe(column);
-    expect(helpers.calculateDescription(column, 17)).toBe(column);
-  }
+      expect(helpers.calculateDescription(0, row)).toBe(expectedLetter);
+      expect(helpers.calculateDescription(25, row)).toBe(expectedLetter);
+    }
+  });
+  test("numeric columns", () => {
+    for (let column of helpers.range(1, 24)) {
+      expect(helpers.calculateDescription(column, 0)).toBe(column);
+      expect(helpers.calculateDescription(column, 17)).toBe(column);
+    }
+  });
 });
 
 test("remove position from an array of positions", () => {
   const ballPosition = [12, 9];
-  
+
   expect(helpers.removeSelf([ballPosition, [12, 5]], [12, 5])).toEqual([[12, 9]]);
   expect(helpers.removeSelf([ballPosition, [12, 5]], [12, 11])).toEqual([[12, 9], [12, 5]]);
 });
--- tests/logic/moveCheckers.test.js
+++ tests/logic/moveCheckers.test.js
@@ -24,74 +24,86 @@ const allPositions = [
 ];
 const ballPosition = [12, 9];
 
-test("can move vertically", () => {
-  expect(privateFunctions.canMoveVertically(allPositions, [12, 10], [12, 9])).toBe(true);
-  expect(privateFunctions.canMoveVertically(allPositions, ballPosition, [12, 11])).toBe(true);
+describe("move vertically", () => {
+  test("can move", () => {
+    expect(privateFunctions.canMoveVertically(allPositions, [12, 10], [12, 9])).toBe(true);
+    expect(privateFunctions.canMoveVertically(allPositions, ballPosition, [12, 11])).toBe(true);
+  });
+  test("can not move past other objects", () => {
+    expect(privateFunctions.canMoveVertically(allPositions, ballPosition, [12, 2])).toBe(false);
+    expect(privateFunctions.canMoveVertically(allPositions, ballPosition, [12, 13])).toBe(false);
+  });
+  test("can not move if the horizontal axes differ", () => {
+    expect(privateFunctions.canMoveVertically(allPositions, ballPosition, [10, 2])).toBe(false);
+    expect(privateFunctions.canMoveVertically(allPositions, [10, 9], [12, 13])).toBe(false);
+  });
 });
-test("can not move vertically past other objects", () => {
-  expect(privateFunctions.canMoveVertically(allPositions, ballPosition, [12, 2])).toBe(false);
-  expect(privateFunctions.canMoveVertically(allPositions, ballPosition, [12, 13])).toBe(false);
+describe("move horizontally", () => {
+  test("can move", () => {
+    expect(privateFunctions.canMoveHorizontally(allPositions, [7, 8], [2, 8])).toBe(true);
+    expect(privateFunctions.canMoveHorizontally(allPositions, [9, 8], [11, 8])).toBe(true);
+  });
+  test("can not move past other objects", () => {
+    expect(privateFunctions.canMoveHorizontally(allPositions, ballPosition, [1, 8])).toBe(false);
+    expect(privateFunctions.canMoveHorizontally(allPositions, [1, 8], [10, 8])).toBe(false);
+  });
+  test("can not move if the vertical axes differ", () => {
+    expect(privateFunctions.canMoveHorizontally(allPositions, ballPosition, [10, 2])).toBe(false);
+    expect(privateFunctions.canMoveHorizontally(allPositions, [10, 9], [12, 13])).toBe(false);
+  });
 });
-test("can not move vertically if the horizontal axes differ", () => {
-  expect(privateFunctions.canMoveVertically(allPositions, ballPosition, [10, 2])).toBe(false);
-  expect(privateFunctions.canMoveVertically(allPositions, [10, 9], [12, 13])).toBe(false);
+describe("move diagonally", () => {
+  test("can move diagonally", () => {
+    expect(privateFunctions.canMoveDiagonally(allPositions, ballPosition, [8, 5])).toBe(true);
+    expect(privateFunctions.canMoveDiagonally(allPositions, ballPosition, [20, 1])).toBe(true);
+    expect(privateFunctions.canMoveDiagonally(allPositions, ballPosition, [19, 16])).toBe(true);
+    expect(privateFunctions.canMoveDiagonally(allPositions, ballPosition, [11, 10])).toBe(true);
+  });
+  test("can not move past other objects", () => {
+    expect(privateFunctions.canMoveDiagonally(allPositions, ballPosition, [5, 2])).toBe(false);
+    expect(privateFunctions.canMoveDiagonally(allPositions, ballPosition, [9, 12])).toBe(false);
+  });
+  test("can not move", () => {
+    expect(privateFunctions.canMoveDiagonally(allPositions, ballPosition, [13, 5])).toBe(false);
+    expect(privateFunctions.canMoveDiagonally(allPositions, ballPosition, [12, 1])).toBe(false);
+  });
 });
 
-test("can move horizontally", () => {
-  expect(privateFunctions.canMoveHorizontally(allPositions, [7, 8], [2, 8])).toBe(true);
-  expect(privateFunctions.canMoveHorizontally(allPositions, [9, 8], [11, 8])).toBe(true);
-});
-test("can not move horizontally past other objects", () => {
-  expect(privateFunctions.canMoveHorizontally(allPositions, ballPosition, [1, 8])).toBe(false);
-  expect(privateFunctions.canMoveHorizontally(allPositions, [1, 8], [10, 8])).toBe(false);
-});
-test("can not move horizontally if the horizontal axes differ", () => {
-  expect(privateFunctions.canMoveHorizontally(allPositions, ballPosition, [10, 2])).toBe(false);
-  expect(privateFunctions.canMoveHorizontally(allPositions, [10, 9], [12, 13])).toBe(false);
-});
-
-test("can move diagonally", () => {
-  expect(privateFunctions.canMoveDiagonally(allPositions, ballPosition, [8, 5])).toBe(true);
-  expect(privateFunctions.canMoveDiagonally(allPositions, ballPosition, [20, 1])).toBe(true);
-  expect(privateFunctions.canMoveDiagonally(allPositions, ballPosition, [19, 16])).toBe(true);
-  expect(privateFunctions.canMoveDiagonally(allPositions, ballPosition, [11, 10])).toBe(true);
-});
-
-test("can move player", () => {
+describe("move player", () => {
   const playerPosition = [22, 7];
-  expect(privateFunctions.canMovePlayer(allPositions, playerPosition, [21, 10])).toBe(true);
-  expect(privateFunctions.canMovePlayer(allPositions, playerPosition, [20, 5])).toBe(true);
-  expect(privateFunctions.canMovePlayer(allPositions, playerPosition, [22, 4])).toBe(true);
-  expect(privateFunctions.canMovePlayer(allPositions, playerPosition, [25, 10])).toBe(true);
+  test("can move", () => {
+    expect(privateFunctions.canMovePlayer(allPositions, playerPosition, [21, 10])).toBe(true);
+    expect(privateFunctions.canMovePlayer(allPositions, playerPosition, [20, 5])).toBe(true);
+    expect(privateFunctions.canMovePlayer(allPositions, playerPosition, [22, 4])).toBe(true);
+    expect(privateFunctions.canMovePlayer(allPositions, playerPosition, [25, 10])).toBe(true);
+  });
+  test("can not move", () => {
+    expect(privateFunctions.canMovePlayer(allPositions, playerPosition, [18, 7])).toBe(false);
+    expect(privateFunctions.canMovePlayer(allPositions, playerPosition, [22, 10])).toBe(false);
+    expect(privateFunctions.canMovePlayer(allPositions, playerPosition, [24, 9])).toBe(false);
+    expect(privateFunctions.canMovePlayer(allPositions, playerPosition, [19, 3])).toBe(false);
+  });
 });
 
-test("can not move player", () => {
-  const playerPosition = [22, 7];
-  expect(privateFunctions.canMovePlayer(allPositions, playerPosition, [18, 7])).toBe(false);
-  expect(privateFunctions.canMovePlayer(allPositions, playerPosition, [22, 10])).toBe(false);
-  expect(privateFunctions.canMovePlayer(allPositions, playerPosition, [24, 9])).toBe(false);
-  expect(privateFunctions.canMovePlayer(allPositions, playerPosition, [19, 3])).toBe(false);
-});
-
-test("is outside of the field", () => {
-  for (let row of helpers.range(helpers.MIN_ROW, helpers.MAX_ROW)) {
-    expect(privateFunctions.isOutsideOfTheField([helpers.MIN_COLUMN, row])).toBe(true);
-    expect(privateFunctions.isOutsideOfTheField([helpers.MAX_COLUMN, row])).toBe(true);
-  }
-  for (let column of helpers.range(helpers.MIN_COLUMN, helpers.MAX_COLUMN)) {
-    expect(privateFunctions.isOutsideOfTheField([column, helpers.MIN_ROW])).toBe(true);
-    expect(privateFunctions.isOutsideOfTheField([column, helpers.MAX_ROW])).toBe(true);
-  }
-});
-
-test("is inside of the field", () => {
-  for (let column of helpers.range(1, 24)) {
-    for (let row of helpers.range(1, 16)) {
-      expect(privateFunctions.isOutsideOfTheField([column, row])).toBe(false);
-      expect(privateFunctions.isOutsideOfTheField([column, row])).toBe(false);
+describe("somewhere on the field", () => {
+  test("outside", () => {
+    for (let row of helpers.range(helpers.MIN_ROW, helpers.MAX_ROW)) {
+      expect(privateFunctions.isOutsideOfTheField([helpers.MIN_COLUMN, row])).toBe(true);
+      expect(privateFunctions.isOutsideOfTheField([helpers.MAX_COLUMN, row])).toBe(true);
     }
-  }
-});
+    for (let column of helpers.range(helpers.MIN_COLUMN, helpers.MAX_COLUMN)) {
+      expect(privateFunctions.isOutsideOfTheField([column, helpers.MIN_ROW])).toBe(true);
+      expect(privateFunctions.isOutsideOfTheField([column, helpers.MAX_ROW])).toBe(true);
+    }
+  });
+  test("inside", () => {
+    for (let column of helpers.range(1, 24)) {
+      for (let row of helpers.range(1, 16)) {
+        expect(privateFunctions.isOutsideOfTheField([column, row])).toBe(false);
+        expect(privateFunctions.isOutsideOfTheField([column, row])).toBe(false);
+      }
+    }
+  });
 });
 
 test("are same position", () => {
