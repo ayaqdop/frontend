f48c5f6e699abc89346f1b6a8864bc80002c6935
Olzhas Askar <olzhas.askar@gmail.com>
2018-04-17 15:20:51 +0200

    Added webpack loaders and a dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const DIST_DIR = path.resolve(__dirname, "./static/dist");
+const DIST_DIR = path.resolve(__dirname, "./build");
 const SRC_DIR = path.resolve(__dirname, "./static");
 
 const config = {
+  devServer: {
+    contentBase: DIST_DIR,
+    compress: true,
+    port: 8000
+  },
   entry: SRC_DIR + "/components/index.js",
   output: {
     path: DIST_DIR,
     filename: "bundle.js",
-    publicPath: DIST_DIR
   },
   module: {
     rules: [
@@ -17,9 +22,29 @@ const config = {
         exclude: /node_modules/,
         include: SRC_DIR,
         loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          "file-loader"
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        hash: true,
+        filename: "index.html",
+        template: "./templates/index.html"
+    })
+  ]
 };
 
 module.exports = config;
