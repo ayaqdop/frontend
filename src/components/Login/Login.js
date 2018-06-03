7337a377255838e3076494a7316542bd991b3c94
Olzhas Askar <olzhas.askar@gmail.com>
2018-06-03 21:25:16 +0200

    Dummy login redirection

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,37 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import "./Login.css";
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authenticated: false
+    };
+    this.login = this.login.bind(this);
+  }
+  login(e) {
+    e.preventDefault();
+    fetch("https://ayaqdop-backend.herokuapp.com/uuid", {
+      method: "POST",
+      credentials: "include" })
+      .then(() => {
+        this.setState({ authenticated: true });
+      });
+  }
+
   render() {
+    if (this.state.authenticated) {
+      return <Redirect to="/game" />;
+    }
+
     return (
       <main className="login">
         <form>
           <input type="text" placeholder="username" />
           <input type="password" placeholder="password" />
-          <button><Link to="/game">login</Link></button>
+          <button onClick={this.login}>login</button>
           <p className="message">Not registered? <a href="#">Create an account</a></p>
         </form>
       </main>
