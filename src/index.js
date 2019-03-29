90d501811bba1d7fd063648834444c11c534c2e0
Olzhas Askar <oas@templafy.com>
2019-03-29 18:38:54 +0100

    Hot reloading only for development

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById("app")
-);
+ReactDOM.render(<App />, document.getElementById("app"));
 
-module.hot.accept();
+if (process.env.NODE_ENV === "development") {
+  module.hot.accept();
+}
