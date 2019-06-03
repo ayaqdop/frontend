c9f49a2f890c49099cc96f0737cbaa862469d73f
pheeria <olzhas.askar@gmail.com>
2019-06-03 11:26:16 +0200

    Use https

--- config/webpack/webpack.dev.js
+++ config/webpack/webpack.dev.js
@@ -1,15 +1,15 @@
-const path = require("path");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
 
-const merge = require("webpack-merge");
-const baseConfig = require("./webpack.common");
-const webpack = require("webpack");
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.common');
+const webpack = require('webpack');
 
 const appDirectory = fs.realpathSync(process.cwd());
-const DIST_DIR = path.resolve(appDirectory, "./build");
+const DIST_DIR = path.resolve(appDirectory, './build');
 
 module.exports = merge(baseConfig, {
-  mode: "development",
+  mode: 'development',
   devServer: {
     contentBase: DIST_DIR,
     compress: true,
@@ -20,7 +20,7 @@ module.exports = merge(baseConfig, {
   },
   plugins: [
     new webpack.DefinePlugin({
-      API: JSON.stringify("http://localhost:8080")
+      API: JSON.stringify('https://localhost:44300')
     }),
     new webpack.HotModuleReplacementPlugin()
   ]
