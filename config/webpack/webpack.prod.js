da97616bb9f07d4d9d7d6e189cfc2a8ff61fe137
Olzhas Askar <olzhas.askar@gmail.com>
2019-11-13 23:05:29 +0100

    Don't uglify

--- config/webpack/webpack.prod.js
+++ config/webpack/webpack.prod.js
@@ -2,19 +2,8 @@ const merge = require("webpack-merge");
 const baseConfig = require("./webpack.common");
 const webpack = require("webpack");
 
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-
 module.exports = merge(baseConfig, {
   mode: "production",
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true
-      })
-    ]
-  },
   plugins: [
     new webpack.DefinePlugin({
       API: JSON.stringify("https://ayaqdop.herokuapp.com")
