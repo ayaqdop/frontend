ec6b3b6826289e8a5533fbf292bb9333d27c249e
Merey <merey.90@gmail.com>
2020-10-27 21:20:12 +0100

    agui agui

--- src/App/index.js
+++ src/App/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Ayaqdop from "../Ayaqdop";
 import Login from "../Login/Login";
+import { MatchMaking } from "../match-making";
 
 export default class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ export default class App extends React.Component {
         <Switch>
           <Route exact path="/" component={Login} />
           <Route path="/game" component={Ayaqdop} />
+          <Route path="/matchmaking" component={MatchMaking} />
         </Switch>
       </BrowserRouter>
     );
---
+++ src/match-making/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './index.css';
+
+export const MatchMaking = () => (
+  <main className="match-making">
+    OHOHOHOOOO
+  </main>
+);
