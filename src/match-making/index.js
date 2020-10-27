e2a3df60afeac7c351ddc05d0a72f346634605e8
Merey Zholdas <merey.90@gmail.com>
2020-10-28 00:20:43 +0100

    Update src/match-making/index.js
    Co-authored-by: Zharzhan Kulmyrza <zharzhan@gmail.com>

--- src/match-making/index.js
+++ src/match-making/index.js
@@ -13,7 +13,7 @@ export const MatchMaking = () => {
   const [matches, setMatches] = useState([])
   const history = useHistory()
 
-  React.useEffect(() => {
+  useEffect(() => {
     auth().onAuthStateChanged(fUser => {
       if (fUser) {
         setUser(fUser)
