c9f7836b77bd8d6f005b7f536bfdb30b5610bebd
Merey Zholdas <merey.90@gmail.com>
2020-10-28 00:21:21 +0100

    Clever matchmaking page (#6)

--- src/match-making/index.js
+++ src/match-making/index.js
@@ -1,42 +1,84 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 
-import "./index.css";
+import initialState from './game.json'
+import { auth } from '../firebase'
+import { db } from '../firebase'
+
+import './index.css'
 
 export const MatchMaking = () => {
-  const [room, setRoom] = React.useState(null);
-  const [showError, setShowError] = React.useState(false);
+  const [showError, setShowError] = useState(null)
+  const [user, setUser] = useState(null)
+  const [matches, setMatches] = useState([])
+  const history = useHistory()
 
-  const onChange = (e) => {
-    setRoom(e.target.value.trim());
-  };
+  useEffect(() => {
+    auth().onAuthStateChanged(fUser => {
+      if (fUser) {
+        setUser(fUser)
+      } else {
+        console.log('NO USER!!!!')
+      }
+    })
+    getMatches()
 
-  const joinRoom = () => {
-    if (room?.length) console.log(`Joinin match: ${room}`);
-    else setShowError(true);
-  };
+    return () => {
+      db.ref(`match`).off()
+    }
+  }, [])
 
-  const createRoom = () => {
-    console.log("Creating new match");
-  };
+  const getMatches = () => {
+    try {
+      db.ref(`match`).on('value', snapshot => {
+        setMatches(Object.keys(snapshot.val()))
+        console.log('snapshot: ', snapshot.val())
+      })
+    } catch (error) {
+      setShowError({ writeError: error.message })
+    }
+  }
+
+  const joinMatch = matchId => {
+    initialState.teams[0].uid = matchId
+    initialState.teams[1].uid = user.uid
+    try {
+      db.ref(`match/${matchId}`).update(initialState)
+      history.push(`/match/${matchId}`)
+    } catch (error) {
+      setShowError({ readError: error.message })
+    }
+  }
+
+  const createMatch = () => {
+    initialState.teams[0].uid = user.uid
+    try {
+      db.ref(`match/${user.uid}`).set(initialState)
+      history.push(`/match/${user.uid}`)
+    } catch (error) {
+      console.log('error: ', error)
+    }
+  }
 
   return (
-    <main className="match-making">
+    <main className='match-making'>
       <form>
-        <button type="button" onClick={createRoom}>
+        <button type='button' onClick={createMatch}>
           Create Match
         </button>
         <hr />
-        <input
-          type="text"
-          placeholder="Match"
-          name="match"
-          onChange={onChange}
-        />
-        <button type="button" onClick={joinRoom}>
-          Join Match
-        </button>
-        {showError && <p className="message">Please specify match to join</p>}
+        <ul>
+          {matches.map(m => (
+            <li key={m}>
+              <button type='button' onClick={() => joinMatch(m)}>
+                Join {m}
+              </button>
+            </li>
+          ))}
+        </ul>
+
+        {showError && <p className='message'>Please specify match to join</p>}
       </form>
     </main>
-  );
-};
+  )
+}
---
+++ src/match/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+import { db } from '../firebase'
+
+export const Match = () => {
+  const { matchId } = useParams()
+
+  React.useEffect(() => {
+    db.ref(`match/${matchId}`).on('value', snapshot => {
+      console.log('snapshot: ', snapshot.val())
+    })
+  }, [])
+  return <h1>MATCH {matchId}</h1>
+}
