1bee59d4a674f52109233b8b35df4a67b24ea517
Merey <merey.90@gmail.com>
2020-10-27 21:53:02 +0100

    matchmacking dummy page

--- src/match-making/index.js
+++ src/match-making/index.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React from "react";
 
-import './index.css';
+import "./index.css";
 
-export const MatchMaking = () => (
-  <main className="match-making">
-    OHOHOHOOOO
-  </main>
-);
+export const MatchMaking = () => {
+  const [room, setRoom] = React.useState(null);
+  const [showError, setShowError] = React.useState(false);
+
+  const onChange = (e) => {
+    setRoom(e.target.value.trim());
+  };
+
+  const joinRoom = () => {
+    if (room?.length) console.log(`Joinin match: ${room}`);
+    else setShowError(true);
+  };
+
+  const createRoom = () => {
+    console.log("Creating new match");
+  };
+
+  return (
+    <main className="match-making">
+      <form>
+        <button type="button" onClick={createRoom}>
+          Create Match
+        </button>
+        <hr />
+        <input
+          type="text"
+          placeholder="Match"
+          name="match"
+          onChange={onChange}
+        />
+        <button type="button" onClick={joinRoom}>
+          Join Match
+        </button>
+        {showError && <p className="message">Please specify match to join</p>}
+      </form>
+    </main>
+  );
+};
