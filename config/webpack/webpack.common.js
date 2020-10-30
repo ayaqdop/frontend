793cc47404bd26c2cf29cbfe9e014e4c9b766f1c
Zharzhan Kulmyrza <zharzhan@gmail.com>
2020-10-30 21:37:10 +0100

    Configure css-modules

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -29,7 +29,22 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
+        exclude: /\.module\.css$/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              import: false,
+              modules: true
+            }
+          }
+        ],
+        include: /\.module\.css$/
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
